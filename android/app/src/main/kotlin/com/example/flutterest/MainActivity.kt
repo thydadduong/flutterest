08aa2b55cc8f6e2e79150acd9d57a37d403ca077
thydadduong <thydaduong7@gmail.com>
2021-01-22 16:43:33 +0700

    upgrade app base on flutter new version

--- android/app/src/main/kotlin/com/example/flutterest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterest/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.flutterest
 
 import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
   }
 }
